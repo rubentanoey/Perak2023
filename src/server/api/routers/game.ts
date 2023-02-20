cebe2071440592dbc7aae1f416641442c81d96b1
edutjie <edutjitrahardja@gmail.com>
2023-02-20 20:39:29 +0700

    Refactor paymentInfo to a new model

--- src/server/api/routers/game.ts
+++ src/server/api/routers/game.ts
@@ -2,7 +2,11 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const gameRouter = createTRPCRouter({
   getGames: publicProcedure.query(async ({ ctx }) => {
-    const games = await ctx.prisma.game.findMany({});
+    const games = await ctx.prisma.game.findMany({
+      include: {
+        paymentInfos: true,
+      },
+    });
     return games;
   }),
 });
