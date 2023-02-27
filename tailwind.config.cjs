384e52ba17062fe0cc35d4a4c9ae91b406902323
Ruben Tanoey <ruben.tanoey@ui.ac.id>
2023-02-27 17:08:13 +0700

    First Commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': {
+          DEFAULT: '#E9DEA6',
+          'dark': '#A39B74',
+          'light': '#F4EFD3',
+        },
+        'green': {
+          DEFAULT: '#A3AA10',
+          'dark': '#71770B',
+          'light': '#C7CC70',
+        },
+        'orange': {
+          DEFAULT: '#FEB048',
+          'dark': '#F36A22',
+          'light': '#EABB76',
+        },
+        'purple': {
+          DEFAULT: '#8C3FD9',
+          'darkest': '#272B52',
+          'dark': '#383D75',
+          'light': '#BB83F2',
+        },
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        decorative: ['Magic Retro', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
